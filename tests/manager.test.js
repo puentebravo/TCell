218f40ba4c5fcde2edbb3faa764785a3fb60e90d
Puentebravo <puentematos@gmail.com>
2021-01-24 19:31:27 -0500

    Wrote test for Manager subclass

--- tests/manager.test.js
+++ tests/manager.test.js
@@ -1,2 +1,22 @@
-import { manager } from "../lib/manager";
+const Employee = require ("../lib/employee")
+const Manager = require ("../lib/manager")
 
+describe ("Manager class", () => {
+    describe('initialization', () => {
+        it('should create an object with name, id, email, and school if provided with valid arguments', () => {
+            const terry = new Manager("terry", 7, "terry@cmail.com", 212)
+
+            expect(terry.empName).toEqual("terry");
+            expect(terry.id).toEqual(7);
+            expect(terry.email).toEqual("terry@cmail.com");
+            expect(terry.officeNo).toEqual(212);
+        });
+    });
+    describe('getRole method', () => {
+        it('Should return Manager as the role', () => {
+            const terry = new Manager("terry", 7, "terry@cmail.com", 212)
+            expect(terry.getRole()).toEqual("Intern")
+        });
+    })
+
+});
