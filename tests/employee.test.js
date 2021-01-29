9c5d18287e93fdc5a7ab1ba1d9a8712a9e3ccdc3
Puentebravo <puentematos@gmail.com>
2021-01-28 23:07:36 -0500

    modified employee test for changes to getRole

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -4,7 +4,7 @@ describe ("employee class", () => {
     // These should all test for whether a new employee object is created returning the correct values, and for whether the methods do what they're supposed to. 
     describe('initialization', () => {
         it('should create an object with name, id, and email if provided with valid arguments', () => {
-            const terry = new Employee("terry", 7, "terry@cmail.com")
+            const terry = new Employee("manager","terry", 7, "terry@cmail.com")
 
             expect(terry.empName).toEqual("terry");
             expect(terry.id).toEqual(7);
@@ -13,25 +13,25 @@ describe ("employee class", () => {
     });
    describe("getName method", () => {
     it('returns the name of the employee', () => {
-        const terry = new Employee("terry", 7, "terry@cmail.com");
+        const terry = new Employee("manager", "terry", 7, "terry@cmail.com");
         expect(terry.getName()).toEqual("terry")
         });
     });
    describe("getId method", () => {
     it('returns the ID of the employee', () => {
-        const terry = new Employee("terry", 7, "terry@cmail.com");
+        const terry = new Employee("manager", "terry", 7, "terry@cmail.com");
         expect(terry.getId()).toEqual(7) 
         });
     });
    describe("getEmail method", () => {
     it('returns the Email of the employee', () => {
-        const terry = new Employee("terry", 7, "terry@cmail.com");
+        const terry = new Employee("manager", "terry", 7, "terry@cmail.com");
         expect(terry.getEmail()).toEqual("terry@cmail.com");
         });
     });
     describe("getRole method", () => {
         it('returns the role of the employee', () => {
-        const terry = new Employee("terry", 7, "terry@cmail.com");
+        const terry = new Employee("Employee", "terry", 7, "terry@cmail.com");
         expect(terry.getRole()).toEqual("Employee");
         });
     });
